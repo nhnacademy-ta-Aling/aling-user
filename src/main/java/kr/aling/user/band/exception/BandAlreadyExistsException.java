b4026f4bad8d8330523700a7342142d95b49d9f4
hobby3530 <jungy3530@gmail.com>
2024-02-02 20:15:41 +0900

    [Feat] 그룹 관련 Exception

---
+++ src/main/java/kr/aling/user/band/exception/BandAlreadyExistsException.java
@@ -0,0 +1,15 @@
+package kr.aling.user.band.exception;
+
+/**
+ * 그룹이 이미 존재할 경우 발생하는 Exception.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+public class BandAlreadyExistsException extends RuntimeException {
+    public static final String MESSAGE = "Band already exists.";
+
+    public BandAlreadyExistsException() {
+        super(MESSAGE);
+    }
+}
