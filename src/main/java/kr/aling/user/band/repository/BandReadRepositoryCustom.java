d744cf1fd4925a3536a14fb1739b386e339902f5
hobby3530 <jungy3530@gmail.com>
2024-02-02 20:14:18 +0900

    [Feat] 그룹 Repository 그룹 생성 API
    - 그룹 생성 API
    - 그룹 중복 검사 API

--- src/main/java/kr/aling/user/band/repository/BandReadRepositoryCustom.java
+++ src/main/java/kr/aling/user/band/repository/BandReadRepositoryCustom.java
@@ -3,10 +3,10 @@ package kr.aling.user.band.repository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 /**
- * Some description here.
+ * 그룹(Band)을 조회 하는 Repository Custom.
  *
- * @author 정유진
- * @since 1.0
+ * @author : 정유진
+ * @since : 1.0
  **/
 @NoRepositoryBean
 public interface BandReadRepositoryCustom {
--- src/main/java/kr/aling/user/band/repository/impl/BandManageRepositoryImpl.java
+++ src/main/java/kr/aling/user/band/repository/impl/BandManageRepositoryImpl.java
@@ -5,10 +5,10 @@ import kr.aling.user.band.repository.BandManageRepositoryCustom;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 /**
- * Some description here.
+ * 그룹(Band)을 관리 하는 Repository 구현체.
  *
- * @author 정유진
- * @since 1.0
+ * @author : 정유진
+ * @since : 1.0
  **/
 public class BandManageRepositoryImpl extends QuerydslRepositorySupport implements BandManageRepositoryCustom {
 
