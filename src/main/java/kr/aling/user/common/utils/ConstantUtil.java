aa8381c6744e6f2ad6cf395f0c7a1f26248d4441
hobby3530 <jungy3530@gmail.com>
2024-02-05 03:28:06 +0900

    [Feat] 상수 유틸 생성
    - 회원 임시 헤더 값 추가

---
+++ src/main/java/kr/aling/user/common/utils/ConstantUtil.java
@@ -0,0 +1,14 @@
+package kr.aling.user.common.utils;
+
+/**
+ * Some description here.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+public class ConstantUtil {
+    public static final String X_TEMP_USER_NO = "X-TEMP-USER-NO";
+
+    private ConstantUtil() {
+    }
+}
