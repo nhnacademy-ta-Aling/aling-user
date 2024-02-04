6e169687b3dea0fcaafba2770ec315310cf8a48d
hobby3530 <jungy3530@gmail.com>
2024-02-05 03:27:43 +0900

    [Feat] 회원 Exception 생성

---
+++ src/main/java/kr/aling/user/user/exception/UserNotFoundException.java
@@ -0,0 +1,16 @@
+package kr.aling.user.user.exception;
+
+/**
+ * 회원을 찾을 수 없을 때 발생 하는 Exception.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+public class UserNotFoundException extends RuntimeException {
+
+    public static final String MESSAGE = "User not found!";
+
+    public UserNotFoundException() {
+        super(MESSAGE);
+    }
+}
