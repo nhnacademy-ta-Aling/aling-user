0366d13abd3cc7764d5d8630983552945ee97971
hobby3530 <jungy3530@gmail.com>
2024-02-05 09:15:00 +0900

    [Docs] Docs 작성

--- src/main/java/kr/aling/user/banduserrole/repository/BandUserRoleReadRepository.java
+++ src/main/java/kr/aling/user/banduserrole/repository/BandUserRoleReadRepository.java
@@ -12,5 +12,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
  **/
 public interface BandUserRoleReadRepository
         extends JpaRepository<BandUserRole, Integer>, BandUserRoleReadRepositoryCustom {
+
+    /**
+     * 그룹 회원 권한 이름으로 그룹 회원 권한을 가져오는 쿼리문.
+     *
+     * @param roleName 그룹 회원 권한 이름
+     * @return 그룹 회원 권한
+     */
     Optional<BandUserRole> findByRoleName(String roleName);
 }
