27bc9b12d3cdee2119ace19d842e61d0cf56f4a6
hobby3530 <jungy3530@gmail.com>
2024-02-19 23:39:44 +0900

    [Refactor] 회원 API 레포지토리 수정
    - 그룹 목록 조회 응답/반환 값에 따른 수정

--- src/main/java/kr/aling/user/user/repository/UserReadRepositoryCustom.java
+++ src/main/java/kr/aling/user/user/repository/UserReadRepositoryCustom.java
@@ -1,7 +1,5 @@
 package kr.aling.user.user.repository;
 
-import java.util.List;
-import kr.aling.user.user.dto.response.GetBandInfoResponseDto;
 import org.springframework.data.repository.NoRepositoryBean;
 
 /**
@@ -20,13 +18,4 @@ public interface UserReadRepositoryCustom {
      * @return 중복 여부(중복시 true)
      */
     Boolean existsByEmail(String email);
-
-    /**
-     * 해당 회원이 가입한 그룹 목록 조회.
-     *
-     * @param userNo 회원 번호
-     * @return 해당 회원이 가입한 그룹 목록
-     */
-    List<GetBandInfoResponseDto> getJoinedBandInfoListByUserNo(Long userNo);
-
 }
--- src/main/java/kr/aling/user/user/repository/impl/UserReadRepositoryImpl.java
+++ src/main/java/kr/aling/user/user/repository/impl/UserReadRepositoryImpl.java
@@ -1,10 +1,5 @@
 package kr.aling.user.user.repository.impl;
 
-import com.querydsl.core.types.Projections;
-import java.util.List;
-import kr.aling.user.band.entity.QBand;
-import kr.aling.user.banduser.entity.QBandUser;
-import kr.aling.user.user.dto.response.GetBandInfoResponseDto;
 import kr.aling.user.user.entity.AlingUser;
 import kr.aling.user.user.entity.QAlingUser;
 import kr.aling.user.user.repository.UserReadRepositoryCustom;
@@ -30,29 +25,5 @@ public class UserReadRepositoryImpl extends QuerydslRepositorySupport implements
         QAlingUser alingUser = QAlingUser.alingUser;
 
         return from(alingUser).select(alingUser.id).where(alingUser.id.eq(email)).fetchCount() >= 1;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<GetBandInfoResponseDto> getJoinedBandInfoListByUserNo(Long userNo) {
-        QAlingUser alingUser = QAlingUser.alingUser;
-        QBandUser bandUser = QBandUser.bandUser;
-        QBand band = QBand.band;
-
-        return from(alingUser)
-                .where(alingUser.userNo.eq(userNo))
-                .where(band.isDelete.isFalse())
-                .innerJoin(bandUser)
-                .on(alingUser.userNo.eq(bandUser.alingUser.userNo))
-                .innerJoin(bandUser.band, band)
-                .select(Projections.constructor(GetBandInfoResponseDto.class,
-                        band.bandNo,
-                        band.name,
-                        band.fileNo,
-                        band.info
-                ))
-                .fetch();
     }
 }
