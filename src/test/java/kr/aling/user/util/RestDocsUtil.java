f579007525d1b341822e49260d80608e6237568b
Gyeongseo Bak <rudtj2966@gmail.com>
2024-02-26 09:21:36 +0900

    [Test] : Rest Docs 설정

---
+++ src/test/java/kr/aling/user/util/RestDocsUtil.java
@@ -0,0 +1,13 @@
+package kr.aling.user.util;
+
+public class RestDocsUtil {
+
+    public static final String VALID = "valid";
+    public static final String REQUIRED = "required";
+
+    public static final String REQUIRED_YES = "O";
+    public static final String REQUIRED_NO = "X";
+
+    private RestDocsUtil() {
+    }
+}
