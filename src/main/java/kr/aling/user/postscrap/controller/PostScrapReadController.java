e5802d32977454dca36e1e623ba72c3d9617886e
SuJeong68 <tidh1640@naver.com>
2024-02-26 23:45:54 +0900

    [Feat] 게시물 스크랩 조회 Controller

---
+++ src/main/java/kr/aling/user/postscrap/controller/PostScrapReadController.java
@@ -0,0 +1,22 @@
+package kr.aling.user.postscrap.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 게시물 스크랩 조회 RestController.
+ *
+ * @author 이수정
+ * @since 1.0
+ */
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/post-scraps")
+@RestController
+public class PostScrapReadController {
+
+    // TODO : 게시물 스크랩 인원 조회
+
+    // TODO : 마이페이지 게시물 스크랩 페이징 조회
+
+}
---
+++ src/main/java/kr/aling/user/recruitpostscrap/controller/RecruitPostScrapReadController.java
@@ -0,0 +1,11 @@
+package kr.aling.user.recruitpostscrap.controller;
+
+public class RecruitPostScrapReadController {
+
+    // TODO : 채용공고 스크랩 인원 조회
+
+    // TODO : 특정 회원의 채용공고 스크랩 중 D-Day 최신순으로 정렬하여 페이징 조회
+
+    // TODO : 마이페이지 채용공고 스크랩 페이징 조회
+
+}
