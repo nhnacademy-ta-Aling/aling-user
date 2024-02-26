00f789747948a25914f744e4f0879d680abce8f1
hobby3530 <jungy3530@gmail.com>
2024-02-26 19:24:16 +0900

    [Feat] 그룹 회원 권한 확인 aspect 작성
    - 관련 어노테이션 및 aspect 작성

---
+++ src/main/java/kr/aling/user/common/annotation/BandCreatorAuth.java
@@ -0,0 +1,18 @@
+package kr.aling.user.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 그룹 회원 Creator 권한 부여를 위한 어노테이션. <br />
+ * Creator 권한은 Creator, Admin, User 권한을 모두 포함합니다.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BandCreatorAuth {
+}
