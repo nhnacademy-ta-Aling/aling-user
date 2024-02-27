65f93683b03fd48c3d1f02a5504ae02f478c5218
SuJeong68 <tidh1640@naver.com>
2024-02-27 18:23:35 +0900

    [Feat] PostFeignClient 내부통신 수정

--- src/main/java/kr/aling/user/common/feign/PostFeignClient.java
+++ src/main/java/kr/aling/user/common/feign/PostFeignClient.java
@@ -5,13 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "aling-post", url = "${aling.openfeign.url}")
+@FeignClient("aling-post")
 public interface PostFeignClient {
 
-    @GetMapping("/post/api/v1/check-post/{postNo}")
-    ResponseEntity<IsExistsPostResponseDto> isExistsPost(
-            @RequestHeader("Authorization") String authorization,
-            @PathVariable("postNo") Long postNo);
+    @GetMapping("/api/v1/check-post/{postNo}")
+    ResponseEntity<IsExistsPostResponseDto> isExistsPost(@PathVariable("postNo") Long postNo);
 }
