7ab9e3914ba1c151178bc357a862c990936fadea
SuJeong68 <tidh1640@naver.com>
2024-03-27 11:00:22 +0900

    [Test] 소셜 로그인 관련 테스트 작성

--- src/test/java/kr/aling/user/user/controller/LoginControllerTest.java
+++ src/test/java/kr/aling/user/user/controller/LoginControllerTest.java
@@ -1,5 +1,8 @@
 package kr.aling.user.user.controller;
 
+import static kr.aling.user.util.RestDocsUtil.REQUIRED;
+import static kr.aling.user.util.RestDocsUtil.REQUIRED_YES;
+import static kr.aling.user.util.RestDocsUtil.VALID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
@@ -10,6 +13,8 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -78,9 +83,48 @@ class LoginControllerTest {
                 preprocessResponse(prettyPrint()),
                 requestFields(
                         fieldWithPath(".email").description("로그인 할 이메일").type(String.class)
-                                .attributes(key("valid").value("최소 3자, 최대 50자")),
+                                .attributes(key(REQUIRED).value(REQUIRED_YES))
+                                .attributes(key(VALID).value("최소 3자, 최대 50자")),
                         fieldWithPath(".password").description("비밀번호").type(String.class)
-                                .attributes(key("valid").value("최소 8자, 최대 20자"))
+                                .attributes(key(REQUIRED).value(REQUIRED_YES))
+                                .attributes(key(VALID).value("최소 8자, 최대 20자"))
+                ),
+                responseHeaders(
+                        headerWithName("X-User-No").description("회원 번호"),
+                        headerWithName("X-User-Role").description("회원 권한 리스트")
+                )));
+    }
+
+    @Test
+    @DisplayName("깃허브 로그인 성공")
+    void github() throws Exception {
+        //given
+        String code = "ABC123!@#";
+
+        LoginResponseDto responseDto = new LoginResponseDto(1L, List.of("ROLE_TEST"));
+
+        when(loginService.github(any())).thenReturn(responseDto);
+
+        //when
+        ResultActions perform = mockMvc.perform(
+                RestDocumentationRequestBuilders.get("/api/v1/login/oauth/github").param("code", code));
+
+        //then
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().exists("X-User-No"))
+                .andExpect(header().string("X-User-No", String.valueOf(responseDto.getUserNo())))
+                .andExpect(header().exists("X-User-Role"))
+                .andExpect(header().string("X-User-Role", objectMapper.writeValueAsString(responseDto.getRoles())));
+
+        //docs
+        perform.andDo(document("oauth-github-login",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestParameters(
+                        parameterWithName("code").description("Github에서 받아온 code")
+                                .attributes(key(REQUIRED).value(REQUIRED_YES))
+                                .attributes(key(VALID).value(""))
                 ),
                 responseHeaders(
                         headerWithName("X-User-No").description("회원 번호"),
--- src/test/java/kr/aling/user/user/service/impl/LoginServiceImplTest.java
+++ src/test/java/kr/aling/user/user/service/impl/LoginServiceImplTest.java
@@ -3,36 +3,58 @@ package kr.aling.user.user.service.impl;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Optional;
+import kr.aling.user.common.properties.OauthProperties;
 import kr.aling.user.user.dto.request.LoginRequestDto;
 import kr.aling.user.user.dto.response.LoginInfoResponseDto;
 import kr.aling.user.user.dto.response.LoginResponseDto;
+import kr.aling.user.user.dto.response.OauthGithubAccessTokenResponseDto;
+import kr.aling.user.user.dto.response.OauthGithubUserInfoResponseDto;
+import kr.aling.user.user.dummy.UserDummy;
+import kr.aling.user.user.entity.AlingUser;
+import kr.aling.user.user.exception.GithubAccessTokenNotExistsException;
+import kr.aling.user.user.exception.SocialEmailNotFoundException;
 import kr.aling.user.user.exception.UserNotFoundException;
 import kr.aling.user.user.repository.UserReadRepository;
 import kr.aling.user.user.service.LoginService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
 
 class LoginServiceImplTest {
 
     private LoginService loginService;
 
     private UserReadRepository userReadRepository;
-
     private PasswordEncoder passwordEncoder;
+
+    private RestTemplate restTemplate;
+    private OauthProperties oauthProperties;
 
     @BeforeEach
     void setUp() {
         userReadRepository = mock(UserReadRepository.class);
         passwordEncoder = mock(PasswordEncoder.class);
 
-        loginService = new LoginServiceImpl(userReadRepository, passwordEncoder);
+        restTemplate = mock(RestTemplate.class);
+        oauthProperties = mock(OauthProperties.class);
+
+        loginService = new LoginServiceImpl(userReadRepository, passwordEncoder, restTemplate, oauthProperties);
     }
 
     @Test
@@ -78,5 +100,147 @@ class LoginServiceImplTest {
 
         // when, then
         assertThatThrownBy(() -> loginService.login(request)).isInstanceOf(UserNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("깃허브 로그인 성공")
+    void github() {
+        // given
+        final String code = "ABC123!@#";
+        final long userNo = 1L;
+        final List<String> roles = List.of("ROLE_TEST");
+
+        AlingUser user = UserDummy.dummy();
+        ReflectionTestUtils.setField(user, "userNo", userNo);
+
+        final String accessTokenUrl = "https://github.com/login/oauth/access_token";
+        final String userUrl = "https://api.github.com/user";
+
+        OauthGithubAccessTokenResponseDto oauthGithubAccessTokenResponseDto = new OauthGithubAccessTokenResponseDto();
+        ReflectionTestUtils.setField(oauthGithubAccessTokenResponseDto, "accessToken", "ABC");
+
+        ResponseEntity accessTokenResponseEntity = mock(ResponseEntity.class);
+        when(restTemplate.exchange(eq(accessTokenUrl), eq(HttpMethod.POST), any(), any(Class.class)))
+                .thenReturn(accessTokenResponseEntity);
+        when(accessTokenResponseEntity.getBody()).thenReturn(oauthGithubAccessTokenResponseDto);
+
+        OauthGithubUserInfoResponseDto oauthGithubUserInfoResponseDto = new OauthGithubUserInfoResponseDto();
+        ReflectionTestUtils.setField(oauthGithubUserInfoResponseDto, "email", user.getEmail());
+
+        ResponseEntity userResponseEntity = mock(ResponseEntity.class);
+        when(restTemplate.exchange(eq(userUrl), eq(HttpMethod.GET), any(), any(Class.class)))
+                .thenReturn(userResponseEntity);
+        when(userResponseEntity.getBody()).thenReturn(oauthGithubUserInfoResponseDto);
+
+        Optional<AlingUser> opsUser = Optional.of(user);
+        when(userReadRepository.findByEmail(anyString())).thenReturn(opsUser);
+
+        when(userReadRepository.findRolesByUserNo(anyLong())).thenReturn(roles);
+
+        // when
+        LoginResponseDto loginResponseDto = loginService.github(code);
+
+        // then
+        assertThat(loginResponseDto).isNotNull();
+        assertThat(loginResponseDto.getUserNo()).isEqualTo(userNo);
+        assertThat(loginResponseDto.getRoles()).isEqualTo(roles);
+    }
+
+    @Test
+    @DisplayName("깃허브 로그인 실패 - 깃허브 AccessToken 발급에 실패한 경우")
+    void github_fail_issueAccessToken() {
+        // given
+        final String code = "ABC123!@#";
+        final String accessTokenUrl = "https://github.com/login/oauth/access_token";
+
+        OauthGithubAccessTokenResponseDto oauthGithubAccessTokenResponseDto = new OauthGithubAccessTokenResponseDto();
+
+        ResponseEntity accessTokenResponseEntity = mock(ResponseEntity.class);
+        when(restTemplate.exchange(eq(accessTokenUrl), eq(HttpMethod.POST), any(), any(Class.class)))
+                .thenReturn(accessTokenResponseEntity);
+        when(accessTokenResponseEntity.getBody()).thenReturn(oauthGithubAccessTokenResponseDto);
+
+        // when, then
+        assertThatThrownBy(() -> loginService.github(code))
+                .isInstanceOf(GithubAccessTokenNotExistsException.class);
+
+        verify(restTemplate, times(1)).exchange(anyString(), any(HttpMethod.class), any(), any(Class.class));
+        verify(userReadRepository, never()).findByEmail(anyString());
+        verify(userReadRepository, never()).findRolesByUserNo(anyLong());
+    }
+
+    @Test
+    @DisplayName("깃허브 로그인 실패 - 깃허브 유저 정보 얻기에 실패한 경우")
+    void github_fail_getUserInfo() {
+        // given
+        final String code = "ABC123!@#";
+        final long userNo = 1L;
+
+        AlingUser user = UserDummy.dummy();
+        ReflectionTestUtils.setField(user, "userNo", userNo);
+
+        final String accessTokenUrl = "https://github.com/login/oauth/access_token";
+        final String userUrl = "https://api.github.com/user";
+
+        OauthGithubAccessTokenResponseDto oauthGithubAccessTokenResponseDto = new OauthGithubAccessTokenResponseDto();
+        ReflectionTestUtils.setField(oauthGithubAccessTokenResponseDto, "accessToken", "ABC");
+
+        ResponseEntity accessTokenResponseEntity = mock(ResponseEntity.class);
+        when(restTemplate.exchange(eq(accessTokenUrl), eq(HttpMethod.POST), any(), any(Class.class)))
+                .thenReturn(accessTokenResponseEntity);
+        when(accessTokenResponseEntity.getBody()).thenReturn(oauthGithubAccessTokenResponseDto);
+
+        OauthGithubUserInfoResponseDto oauthGithubUserInfoResponseDto = new OauthGithubUserInfoResponseDto();
+
+        ResponseEntity userResponseEntity = mock(ResponseEntity.class);
+        when(restTemplate.exchange(eq(userUrl), eq(HttpMethod.GET), any(), any(Class.class)))
+                .thenReturn(userResponseEntity);
+        when(userResponseEntity.getBody()).thenReturn(oauthGithubUserInfoResponseDto);
+
+        // when, then
+        assertThatThrownBy(() -> loginService.github(code))
+                .isInstanceOf(SocialEmailNotFoundException.class);
+
+        verify(restTemplate, times(2)).exchange(anyString(), any(HttpMethod.class), any(), any(Class.class));
+        verify(userReadRepository, never()).findByEmail(anyString());
+        verify(userReadRepository, never()).findRolesByUserNo(anyLong());
+    }
+
+    @Test
+    @DisplayName("깃허브 로그인 실패 - 얻어온 이메일이 DB상에 존재하지 않는 경우")
+    void github_fail_emailNotFound() {
+        // given
+        final String code = "ABC123!@#";
+        final long userNo = 1L;
+
+        AlingUser user = UserDummy.dummy();
+        ReflectionTestUtils.setField(user, "userNo", userNo);
+
+        final String accessTokenUrl = "https://github.com/login/oauth/access_token";
+        final String userUrl = "https://api.github.com/user";
+
+        OauthGithubAccessTokenResponseDto oauthGithubAccessTokenResponseDto = new OauthGithubAccessTokenResponseDto();
+        ReflectionTestUtils.setField(oauthGithubAccessTokenResponseDto, "accessToken", "ABC");
+
+        ResponseEntity accessTokenResponseEntity = mock(ResponseEntity.class);
+        when(restTemplate.exchange(eq(accessTokenUrl), eq(HttpMethod.POST), any(), any(Class.class)))
+                .thenReturn(accessTokenResponseEntity);
+        when(accessTokenResponseEntity.getBody()).thenReturn(oauthGithubAccessTokenResponseDto);
+
+        OauthGithubUserInfoResponseDto oauthGithubUserInfoResponseDto = new OauthGithubUserInfoResponseDto();
+        ReflectionTestUtils.setField(oauthGithubUserInfoResponseDto, "email", user.getEmail());
+
+        ResponseEntity userResponseEntity = mock(ResponseEntity.class);
+        when(restTemplate.exchange(eq(userUrl), eq(HttpMethod.GET), any(), any(Class.class)))
+                .thenReturn(userResponseEntity);
+        when(userResponseEntity.getBody()).thenReturn(oauthGithubUserInfoResponseDto);
+
+        // when, then
+        assertThatThrownBy(() -> loginService.github(code))
+                .isInstanceOf(SocialEmailNotFoundException.class);
+
+        verify(restTemplate, times(2)).exchange(anyString(), any(HttpMethod.class), any(), any(Class.class));
+        verify(userReadRepository, times(1)).findByEmail(anyString());
+        verify(userReadRepository, never()).findRolesByUserNo(anyLong());
     }
 }
